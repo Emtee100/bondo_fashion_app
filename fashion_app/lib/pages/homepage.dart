03044cfd4c5c34902eb61e6200e3f5c17827f1e0
Emtee100 <mt9510037@gmail.com>
2023-02-09 22:52:06 +0300

    Started the categories part of UI

---
+++ fashion_app/lib/pages/homepage.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> categories = ['Shop Men', 'Shop Women', 'Shop Kid\'s', 'Sale'];
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0, top: 4.0),
+                child: Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const FaIcon(FontAwesomeIcons.bars),
+                ),
+              ),
+
+              //title
+              Text("BRONDO",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600, fontSize: 20)),
+              //cart
+              Padding(
+                padding: const EdgeInsets.only(right: 15.0, top: 4.0),
+                child: Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const FaIcon(FontAwesomeIcons.bagShopping),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          //categories
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20.0),
+            height: 55,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _selectedIndex == index;
+                    });
+                    print(_selectedIndex);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    padding: const EdgeInsets.all(18),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30.0),
+                      color: _selectedIndex == index
+                          ? Colors.orange.shade700
+                          : Colors.white,
+                      border: _selectedIndex == index? Border.all(color: Colors.orange.shade700):Border.all()
+                    ),
+                    child: Text(
+                      categories[index],
+                      style: GoogleFonts.poppins(
+                        color:
+                            _selectedIndex == index ? Colors.white : Colors.black,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+
+          //new arrival banner
+
+          //actual new arrivals
+
+          //most popular banner
+
+          //actual most popular
+        ],
+      )),
+    );
+  }
+}
